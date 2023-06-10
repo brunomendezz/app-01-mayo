42a9e860cd62fc11cd744293c4ffd2536b9db84f
bruno <brunomartinmendez@gmail.com>
2023-06-10 19:50:44 -0300

    Implementacion de firebase crash y analitycs , diseño de home screen .. a partir de ahora hare ramas para trabajar de manera prolija

---
+++ app/src/main/java/com/mayorista/oscar/mayoristaoscar/domain/GetPdf.kt
@@ -0,0 +1,12 @@
+package com.mayorista.oscar.mayoristaoscar.domain
+
+import android.graphics.pdf.PdfDocument
+import com.mayorista.oscar.mayoristaoscar.data.PdfRepository
+import javax.inject.Inject
+
+class GetNewPdf @Inject constructor(var repo: PdfRepository) {
+
+    suspend fun getPdf(): PdfDocument? {
+        return repo.getPdf()
+    }
+}
--- app/src/main/java/com/mayorista/oscar/mayoristaoscar/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mayorista/oscar/mayoristaoscar/ui/screens/HomeScreen.kt
@@ -1,20 +1,21 @@
 package com.mayorista.oscar.mayoristaoscar.ui.screens
 
-import android.graphics.pdf.PdfRenderer
-import android.net.Uri
-import androidx.compose.foundation.Image
+import android.graphics.drawable.shapes.RectShape
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,29 +24,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.net.toFile
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.mayorista.oscar.mayoristaoscar.navigation.AppScreens
-import java.io.File
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
@@ -57,7 +49,7 @@ fun HomeScreen(navController: NavHostController) {
             .fillMaxSize()
             .padding(it)){
 
-            DegradedBackground()
+            ContentHomeScreen()
 
         }
 
@@ -118,28 +110,95 @@ fun TopAppBarActionButton(
     }
 }
 
-@Composable
-fun DegradedBackground() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(Color(0xFFE0070F), Color(0xFFFFFFFF)),
-                    startY = 0f,
-                    endY = 450f
-                )
-            )
-    ) {
-       ContentHomeScreen()
-    }
-}
 
 @Composable
 fun ContentHomeScreen(){
-    Card(modifier = Modifier
-        .width(250.dp)){
-        Text(text = "HOLA")
+    Box(modifier =Modifier
+        .fillMaxSize()
+        .background(   brush = Brush.verticalGradient(
+            colors = listOf(Color(0xFFE0070F), Color(0xFFFFFFFF)),
+            startY = 0f,
+            endY = 200f))){
+        LazyColumn(
+            Modifier
+                .align(Alignment.Center)
+                .fillMaxSize()
+          ){
+
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(Color(0xFFE0070F), Color(0xFFFFFFFF)),
+                                startY = 0f,
+                                endY = 200f
+                            )
+                        )
+                ) {
+                    CardPdf(Modifier.align(Alignment.TopCenter))
+                }
+            }
+                item{
+                    Box(modifier =Modifier
+                        .fillMaxWidth()
+                        .background(Color.White)
+                        ) {
+                        CardPdf(Modifier.align(Alignment.TopCenter))
+                    }
+                }
+            item{
+                Box(modifier =Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                ) {
+                    CardPdf(Modifier.align(Alignment.TopCenter))
+                }
+            }
+            item{
+                Box(modifier =Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                ) {
+                    CardPdf(Modifier.align(Alignment.TopCenter))
+                }
+            }
+            item{
+                Box(modifier =Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                ) {
+                    CardPdf(Modifier.align(Alignment.TopCenter))
+                }
+            }
+
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+@Composable
+fun CardPdf(modifier: Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(15.dp)
+            .shadow(5.dp), // Ajusta el valor de elevación según tus preferencias
+    ) {
+        Text(
+            text = "SERA QUE PODREMOS LOGRAR NUESTRO OBJETIVO? NO LO SE .. SOLO SE QUE NO SE NADA",
+            textAlign = TextAlign.Center,
+            color = Color.Black
+        )
     }
 }
-
