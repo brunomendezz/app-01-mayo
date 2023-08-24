17faa58d3297cd7890367beedd152bb5a5d067d9
bruno <brunomartinmendez@gmail.com>
2023-08-24 18:36:32 -0300

    24-08-2023

--- app/src/main/java/com/mayorista/oscar/mayoristaoscar/data/repos/MarcadorRepository.kt
+++ app/src/main/java/com/mayorista/oscar/mayoristaoscar/data/repos/MarcadorRepository.kt
@@ -7,6 +7,8 @@ import javax.inject.Inject
 class MarcadorRepository @Inject constructor(){
 
     companion object UbicacionesDataProvider {
+
+        //LOS VA A TOMAR DE UNA API O FIREBASE PARA PODER CAMBIAR O AGREGAR UBICACIONES.
         var ubicaciones = mutableListOf(
             Marcador("TAPIALES", LatLng(-34.70054831905432, -58.50331634388686)),
             Marcador("PONTEVEDRA", LatLng(-34.74277906543054, -58.693338946004005)),
